a95f1dc1698b930af547a20dc17f63cdfdbf7d4c
liangpengyv <mrliangpengyv@gmail.com>
2020-07-27 19:07:33 +0800

    添加全局 CSS 预设

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <div id="app">
-    <h1>app</h1>
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="less">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="header">
+    <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped lang="less">
+
+</style>
