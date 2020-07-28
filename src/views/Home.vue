9a61db670bfb3d17f8781bda72e502dfd5898c7c
liangpengyv <mrliangpengyv@gmail.com>
2020-07-28 16:44:49 +0800

    完成首页布局样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,124 @@
 <template>
   <div id="home">
     <Header></Header>
-    <div>
-      <h1>知乎专栏</h1>
-      <h2>随心写作，自由表达</h2>
-      <button>开始写文章</button>
-      <button>申请开通专栏
-        <span style="display:inline-flex;align-items:center">​<svg class="Zi Zi--ArrowRight" fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path d="M9.218 16.78a.737.737 0 0 0 1.052 0l4.512-4.249a.758.758 0 0 0 0-1.063L10.27 7.22a.737.737 0 0 0-1.052 0 .759.759 0 0 0-.001 1.063L13 12l-3.782 3.716a.758.758 0 0 0 0 1.063z" fill-rule="evenodd"></path></svg></span>
+    <div class="home-top">
+      <h1 class="title">知乎专栏</h1>
+      <h2 class="subtitle text-center">随心写作，自由表达</h2>
+      <button class="start-write">开始写文章</button>
+      <button class="apply-for">申请开通专栏<span style="display:inline-flex;align-items:center">​<svg fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path d="M9.218 16.78a.737.737 0 0 0 1.052 0l4.512-4.249a.758.758 0 0 0 0-1.063L10.27 7.22a.737.737 0 0 0-1.052 0 .759.759 0 0 0-.001 1.063L13 12l-3.782 3.716a.758.758 0 0 0 0 1.063z" fill-rule="evenodd"></path></svg></span>
       </button>
     </div>
-    <div>
-      <h3>专栏 · 发现</h3>
-      <div>
-        <a>
-          <img width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
-          <div>
-            <div>外汇的逻辑</div>
-            <div>我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+    <div class="home-middle">
+      <h3 class="section-title center-block text-center">专栏 · 发现</h3>
+      <div class="container center-block">
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
           </div>
-          <div>
+          <div class="meta">
             1,785
             人关注 |
             3
             篇文章
           </div>
-          <button>进入专栏</button>
+          <button class="enter-into">进入专栏</button>
+        </a>
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+          </div>
+          <div class="meta">
+            1,785
+            人关注 |
+            3
+            篇文章
+          </div>
+          <button class="enter-into">进入专栏</button>
+        </a>
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+          </div>
+          <div class="meta">
+            1,785
+            人关注 |
+            3
+            篇文章
+          </div>
+          <button class="enter-into">进入专栏</button>
+        </a>
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+          </div>
+          <div class="meta">
+            1,785
+            人关注 |
+            3
+            篇文章
+          </div>
+          <button class="enter-into">进入专栏</button>
+        </a>
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+          </div>
+          <div class="meta">
+            1,785
+            人关注 |
+            3
+            篇文章
+          </div>
+          <button class="enter-into">进入专栏</button>
+        </a>
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+          </div>
+          <div class="meta">
+            1,785
+            人关注 |
+            3
+            篇文章
+          </div>
+          <button class="enter-into">进入专栏</button>
+        </a>
+        <a class="item text-center">
+          <img class="avatar" width="48" height="48" src="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_xs.jpg" srcset="https://pic4.zhimg.com/v2-463d6baa72be45c295632a968d0e4053_l.jpg 2x" alt="专栏图像">
+          <div class="info">
+            <div class="title">外汇的逻辑</div>
+            <div class="description">我曾在芬兰央行任职宏观研究助理，长期以来对外汇研究和交易都是充满了热爱。不管从理论还是到实战交易，我都希望能写出来跟大家讨论。</div>
+          </div>
+          <div class="meta">
+            1,785
+            人关注 |
+            3
+            篇文章
+          </div>
+          <button class="enter-into">进入专栏</button>
         </a>
       </div>
-      <button>
+      <button class="change-button center-block">
         <span style="display:inline-flex;align-items:center">​<svg class="Zi Zi--Refresh Button-zi" fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path d="M20 12.878C20 17.358 16.411 21 12 21s-8-3.643-8-8.122c0-4.044 3.032-7.51 6.954-8.038.034-1.185.012-1.049.012-1.049-.013-.728.461-1.003 1.057-.615l3.311 2.158c.598.39.596 1.026 0 1.418l-3.31 2.181c-.598.393-1.08.12-1.079-.606 0 0 .006-.606-.003-1.157-2.689.51-4.675 2.9-4.675 5.708 0 3.21 2.572 5.822 5.733 5.822 3.163 0 5.733-2.612 5.733-5.822 0-.633.51-1.148 1.134-1.148.625 0 1.133.515 1.133 1.148" fill-rule="evenodd"></path></svg></span>
         换一换
       </button>
     </div>
-    <div>
-      <h3>在知乎创作</h3>
-      <button>申请专栏</button>
-      <footer class="ColumnHomeBottom-footer">
+    <div class="home-bottom text-center">
+      <h3 class="section-title center-block">在知乎创作</h3>
+      <button class="apply-for">申请专栏</button>
+      <footer class="footer">
         <span>
           ©
           2020
@@ -81,5 +164,203 @@
 </script>
 
 <style scoped lang="less">
+  #home {
+    background: #fbfcfc;
 
+    .home-top {
+      background: url(https://static.zhihu.com/heifetz/assets/patterns.c72a480c.png) 50% no-repeat;
+      height: 47rem;
+      margin-top: 5.2rem;
+
+      &::before {
+        background: url(https://static.zhihu.com/heifetz/assets/bg@2x.033e5b2d.png) repeat-x;
+        background-size: 2rem 45rem;
+        position: absolute;
+        content: "";
+        width: 100%;
+        height: 47rem;
+        left: 0;
+      }
+
+      .title {
+        background: url(https://static.zhihu.com/heifetz/assets/logo.84666bea.png) 50% no-repeat;
+        background-size: 12.9rem 17.9rem;
+        position: absolute;
+        width: 12.9rem;
+        height: 0;
+        left: 50%;
+        top: 20.4rem;
+        margin: 0 0 0 -6.5rem;
+        padding-top: 17.9rem;
+        overflow: hidden;
+      }
+
+      .subtitle {
+        position: absolute;
+        width: 100%;
+        top: 40.2rem;
+        margin: 0;
+        color: #1a1a1a;
+        font-size: 1.8rem;
+        font-weight: 300;
+        line-height: 2.4rem;
+        letter-spacing: .9rem;
+      }
+
+      .start-write {
+        width: 12rem;
+        height: 3.8rem;
+        position: absolute;
+        top: 46.5rem;
+        left: 50%;
+        margin-left: -5.1rem;
+        padding: 0 1.6rem;
+        font-size: 1.4rem;
+        line-height: 3.6rem;
+        background: none;
+        color: #444;
+        border: .1rem solid #444;
+        border-radius: .3rem;
+      }
+
+      .apply-for {
+        position: absolute;
+        top: 51.8rem;
+        left: 50%;
+        margin-left: -5.2rem;
+        padding: 0 1.6rem;
+        font-size: 1.4rem;
+        line-height: 1.2rem;
+        background: none;
+        color: #11a668;
+        border: none;
+      }
+    }
+
+    .home-middle {
+      margin-top: 4.7rem;
+
+      .section-title {
+        width: 88.8rem;
+        font-size: 1.4rem;
+        line-height: 2.4rem;
+        font-weight: 600;
+        color: #1a1a1a;
+      }
+
+      .container {
+        width: 88.8rem;
+        margin-top: 3.6rem;
+        display: flex;
+        flex-wrap: wrap;
+
+        .item {
+          width: 20.6rem;
+          margin: 0 .8rem 1.6rem;
+          padding: 2.6rem 0 2.3rem;
+          background: white;
+          border-radius: .4rem;
+          cursor: pointer;
+          box-shadow: 0 .2rem 1rem 0 #E2E2E2;
+
+          .avatar {
+            border-radius: 50%;
+          }
+
+          .info {
+            padding: 0 1.6rem;
+
+            .title {
+              margin-top: 1.6rem;
+              font-size: 1.6rem;
+              font-weight: 600;
+              color: #1a1a1a;
+            }
+
+            .description {
+              margin-top: .7rem;
+              font-size: 1.4rem;
+              line-height: 2.1rem;
+              color: grey;
+              display: -webkit-box;
+              -webkit-box-orient: vertical;
+              -webkit-line-clamp: 2;
+              overflow: hidden;
+            }
+          }
+
+          .meta {
+            color: grey;
+            font-size: 1.4rem;
+            margin-top: 1.4rem;
+          }
+
+          .enter-into {
+            margin-top: 1.5rem;
+            padding: 0 1.6rem;
+            font-size: 1.4rem;
+            line-height: 3.2rem;
+            color: #11a668;
+            background: none;
+            border: .1rem solid #11a668;
+            border-radius: .3rem;
+          }
+        }
+      }
+
+      .change-button {
+        width: 9.8rem;
+        margin-top: 2rem;
+        padding: 0 1.6rem;
+        font-size: 1.4rem;
+        line-height: 3.2rem;
+        border: .1rem solid #444;
+        border-radius: .3rem;
+        background-color: transparent;
+        color: #444;
+      }
+    }
+
+    .home-bottom {
+      margin-top: 4.8rem;
+      padding-top: 13.4rem;
+      background: url(https://static.zhihu.com/heifetz/assets/bg@2x.033e5b2d.png) repeat-x;
+      background-size: 2rem 45rem;
+
+      .section-title {
+        width: 34.5rem;
+        padding: 2.6rem 0;
+        border: solid #97ffd8;
+        border-width: .1rem 0;
+        font-size: 1.8rem;
+        font-weight: 300;
+        letter-spacing: 1.4rem;
+      }
+
+      .apply-for {
+        height: 3.8rem;
+        width: 12rem;
+        margin-top: 4rem;
+        padding: 0 1.6rem;
+        line-height: 3.6rem;
+        font-size: 1.4rem;
+        color: #444;
+        background: none;
+        border: .1rem solid #444;
+        border-radius: .3rem;
+      }
+
+      .footer {
+        color: grey;
+        font-size: 1.2rem;
+        line-height: 2.2rem;
+        margin: 8rem 0 6rem;
+        padding: 0 1.2rem;
+
+        a {
+          color: grey;
+        }
+      }
+    }
+  }
 </style>
